187fe6f9ef49426b6d1fb906a7e7af1d66ee15f6
fotino21 <ataturhan21@gmail.com>
2024-04-30 15:36:59 -0500

    body about projects section added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Header from './components/Header';
+import Body from './components/Body';
+import About from './components/About';
+import Projects from './components/Projects';
 import './App.css';
 
 const App = () => {
   return (
     <div id="app" className="App">
       <Header />
+      <Body />
+      <About />
+      <Projects />
     </div>
   );
 }
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,1 +1,26 @@
-// Create your Body component here
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import Avatar from '../assets/pfp.png';
+
+const Body = () => {
+    return (
+        <div id="body" className="body">
+            <div className="body-container">
+                <div className="body-profile">
+                    <img className="body-img" alt='avatar' src={Avatar} />
+
+                    <div className="body-content">
+                        <div className="body-headline">Ata Turhan</div>
+                        <div className="body-text">Machine Learning Software Engineer</div>
+                    </div>
+
+                    <div className="body-icons">
+                        <a href="https://github.com/ata-turhan" target="_blank" rel="noreferrer" className="icon-link"><i><FaGithub /></i> </a>
+                        <a href="https://www.linkedin.com/in/ataturhan/" target="_blank" rel="noreferrer" className="icon-link"><i><FaLinkedin /></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Body;
