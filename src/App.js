ea02411d09d9afe5c0d0453fb999f313204c5bca
fotino21 <ataturhan21@gmail.com>
2024-04-30 18:52:24 -0500

    feature: skills and footer added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from './components/Header';
 import Body from './components/Body';
 import About from './components/About';
 import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Footer from './components/Footer';
 import './App.css';
 
 const App = () => {
@@ -11,6 +13,8 @@ const App = () => {
       <Body />
       <About />
       <Projects />
+      <Skills />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,22 @@
-// Create your Footer component here
+import { FaMedium } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <footer id="footer" className="footer">
+            <div className="footer-contact-info">
+                <h1 className="footer-heading">Connect With Me</h1>
+                <p className="footer-contact-access">
+                <a href="mailto:ataturhan21@gmail.com">Email: ataturhan21@gmail.com</a>
+                </p>
+            </div>
+            <div>
+                <h1>Social Links</h1>
+                <div className="social-icons">
+                    <a href="https://medium.com/@ataturhan"><i><FaMedium /></i></a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
